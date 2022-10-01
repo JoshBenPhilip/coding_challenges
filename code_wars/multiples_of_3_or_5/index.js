9862981952185246d9a67d051d7496c1b739c3d5
JoshBenPhilip <joshben4c@gmail.com>
2022-10-01 17:19:09 -0400

    second commit

--- code_wars/multiples_of_3_or_5/index.js
+++ code_wars/multiples_of_3_or_5/index.js
@@ -29,34 +29,47 @@
 // console.log(testNum(-5));
 // // expected output: "NOT positive"
 
-// const multiples = (multipleOfThisNum, belowThisNum) => {
-//   const numOfMultiplesContained = Math.floor(belowThisNum / multipleOfThisNum);
-//   let arrOfMultiples = [];
-//   for (let i = 1; i <= numOfMultiplesContained; i++) {
-//     let val = multipleOfThisNum * i;
-//     arrOfMultiples.push(val);
-//   }
+const multiples = (multipleOfThisNum, belowThisNum) => {
+  const numOfMultiplesContained = Math.floor(belowThisNum / multipleOfThisNum);
+  let arrOfMultiples = [];
+  for (let i = 1; i <= numOfMultiplesContained; i++) {
+    let val = multipleOfThisNum * i;
+    arrOfMultiples.push(val);
+  }
 
-//   return arrOfMultiples;
-// };
+  return arrOfMultiples;
+};
 
 // console.log(multiples(3, 15));
 
-function solution(number) {
-  const belowThisNum = number;
-  const multiples = (multipleOfThisNum, belowThisNum) => {
-    const numOfMultiplesContained = Math.floor(
-      belowThisNum / multipleOfThisNum
-    );
-    let arrOfMultiples = [];
-    for (let i = 0; i <= numOfMultiplesContained; i++) {
-      let val = multipleOfThisNum * i;
-      arrOfMultiples.push(i);
-    }
+const array1 = multiples(3, 15);
+const array2 = multiples(5, 15);
 
-    return arrOfMultiples;
-  };
+console.log(array1.forEach{function funct(element){if array2.includes(element)}});
 
-  const array1 = multiples(3, belowThisNum);
-  const array2 = multiples(5, belowThisNum);
-}
+// function solution(number) {
+//   const belowThisNum = number;
+//   const multiples = (multipleOfThisNum, belowThisNum) => {
+//     const numOfMultiplesContained = Math.floor(
+//       belowThisNum / multipleOfThisNum
+//     );
+//     let arrOfMultiples = [];
+//     for (let i = 0; i <= numOfMultiplesContained; i++) {
+//       let val = multipleOfThisNum * i;
+//       arrOfMultiples.push(i);
+//     }
+
+//     return arrOfMultiples;
+//   };
+
+//   const array1 = multiples(3, belowThisNum);
+//   const array2 = multiples(5, belowThisNum);
+
+// const array3 = array1 + array2 - array1.includes(array2)
+
+// function numsToDeleteExtraOf (arrayA, arrayB){const NumsToDeleteExtraOf = []
+//   function loop (arrayA, arrayB) {for (let i=0; i< arrayA.length; i++) { for ( let j=0; j<arrayB.length; j++){ if (i == j) { numsToDeleteExtraOf.push(i)}}
+// }
+
+// const array3 = array1 + array2 -
+// }
